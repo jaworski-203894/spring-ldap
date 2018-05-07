912ad50ae2ae8aecb2d6f1e35810a828c19a40ca
Marcin Jaworski <m.a.jaworski96@gmail.com>
2018-05-07 23:19:42 +0200

    Little secruity fix.

--- ldap/src/main/java/hello/WebSecurityConfig.java
+++ ldap/src/main/java/hello/WebSecurityConfig.java
@@ -16,8 +16,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/public/login").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 // We filter the api/login requests
